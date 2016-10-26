b8c7ea7c59a135566eec4ac3cd70fedd62959900
Gorkapromero <drmoriartydiu@gmail.com>
2016-10-26 12:25:06 +0200

    Dino exportado - Mascotas islas - retoques

---
+++ Assets/Control_animaciondino_saludar.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Control_animaciondino_saludar : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+	
+	void OnTriggerEnter(Collider coli)
+		
+	{
+		if (coli.gameObject.name == "Chico_TEAPlay") 
+		{
+			GameObject dino = GameObject.Find ("Dinoi_animaciones_v3");
+			Animator dino_animator = dino.GetComponent<Animator> ();
+			dino_animator.SetBool ("bAccionDino", true);
+		}
+		
+	}
+	void OnTriggerExit(Collider coli)
+	{
+
+		
+	}
+}
