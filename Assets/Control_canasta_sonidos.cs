eeaeef8e67df900217415756caa5fda786e1ca42
Gorkapromero <drmoriartydiu@gmail.com>
2016-11-07 11:58:01 +0100

    Prueba_sonidos_canasta

---
+++ Assets/Control_canasta_sonidos.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+
+
+
+public class Control_canasta_sonidos : MonoBehaviour 
+{
+	public AudioSource encestar;
+	public AudioSource musicacierto;
+
+	void start () 
+	{
+		AudioSource[] SonidosCancha = GetComponents<AudioSource> ();
+		encestar = SonidosCancha [0];
+		musicacierto = SonidosCancha [1];
+
+	}
+	void Encestar() 
+	{
+		encestar.Play (); 
+	}
+	void Musicaacierto()
+	{
+		musicacierto.Play (); 
+	}
+
+}
