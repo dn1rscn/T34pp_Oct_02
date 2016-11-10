d37905c7f814e1a936720d7b427f9a34f23f4028
JPalacio3D <jorge.jp92@gmail.com>
2016-11-10 19:09:52 +0100

    "pause" abrir menu

--- Assets/Scripts/00-Menus/Interface_Mundo.cs
+++ Assets/Scripts/00-Menus/Interface_Mundo.cs
@@ -5,17 +5,22 @@ public class Interface_Mundo : MonoBehaviour
 {
 	control_datosGlobalesPersonalizacion cdgP;
 
+	NavMeshAgent agente;
+
 	public GameObject Bcerrar;
 	public GameObject Babrir;
 
 	// Use this for initialization
 	void Start () 
 	{
-		GameObject.Find ("Chico_TEAPlay").GetComponent<ControlProtaMouse_2> ().enabled = true;
+		//GameObject.Find ("Chico_TEAPlay").GetComponent<ControlProtaMouse_2> ().enabled = true;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+		agente = GameObject.Find ("Chico_TEAPlay").GetComponent<NavMeshAgent> ();
+		//print (agente.remainingDistance);
 	
 	}
 	public void cerrar()
@@ -28,19 +33,26 @@ public class Interface_Mundo : MonoBehaviour
 	}
 	public void abrir()
 	{
+		GameObject.Find ("Chico_TEAPlay").GetComponent<ControlProtaMouse_2> ().enabled = false;
 		Bcerrar.SetActive (true);
 		Babrir.SetActive (false);
+		agente = GameObject.Find ("Chico_TEAPlay").GetComponent<NavMeshAgent> ();
 		GameObject.Find ("cuadro").GetComponent<Animator> ().Play ("abrir");
-		GameObject.Find ("Chico_TEAPlay").GetComponent<ControlProtaMouse_2> ().enabled = false;
+		agente.destination = GameObject.Find ("Chico_TEAPlay").transform.position;
+		GameObject.Find ("Chico_TEAPlay").GetComponent<Animator> ().SetBool("andar",false);
+		
+
 		//Time.timeScale = 0;
 	}
 	public void Mapa()
 	{
+		GameObject.Find ("Chico_TEAPlay").GetComponent<ControlProtaMouse_2> ().enabled = true;
 		Time.timeScale = 1;
 		Application.LoadLevel ("Mapa");
 	}
 	public void PersonalizacionJuego()
 	{
+		GameObject.Find ("Chico_TEAPlay").GetComponent<ControlProtaMouse_2> ().enabled = true;
 		cdgP = GameObject.Find ("datosGlobalesPersonalizacion").GetComponent<control_datosGlobalesPersonalizacion> ();
 		Time.timeScale = 1;
 		//print ("hola");
