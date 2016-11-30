1391fbacffce40d8c8817c5ad5767aa40c31a954
Gorkapromero <drmoriartydiu@gmail.com>
2016-11-30 09:38:05 +0100

    Sonidos -  ejers (cont)

---
+++ Assets/Control_sonidos_robot.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(AudioSource))]
+
+public class Control_sonidos_robot : MonoBehaviour
+{
+	public AudioSource fallorobot; 
+	public AudioSource aciertorobot;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+	void Aciertorobot ()
+	{
+		aciertorobot.Play (); 
+	}
+	void Fallorobot ()
+	{
+		fallorobot.Play (); 
+	}
+}
